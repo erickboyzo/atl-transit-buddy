b699bd23e8b11dddbc68c4c456ce87293def09b2
erick.boyzo <erick.boyzo@ihg.com>
2018-07-03 12:40:40 -0400

    add app footer and clean up some styling

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  personalLinks = [
+    { icon: 'fa-linkedin', link: 'https://www.linkedin.com/in/erick-boyzo-258023a1' },
+    { icon: 'fa-github', link: 'https://github.com/erickboyzo' },
+    { icon: 'fa-code', link: 'https://bitbucket.org/erickby/marta_transit_buddy/src/master/' }
+  ];
+
+  currentYear = new Date().getFullYear();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+    openLink(link) {
+    window.open(link, '_blank');
   }
 
 }
