5796925b2ce21f14dcc83ebeca0d4c681a4ff041
Erick Boyzo <erickb59@gmail.com>
2019-08-13 21:45:50 -0400

    fix real time endpoint not working

--- src/lambda/realTimeTrain.js
+++ src/lambda/realTimeTrain.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiKey = process.env.API_KEY;
+const apikey = process.env.API_KEY;
 const baseApi = 'http://developer.itsmarta.com/RealtimeTrain/RestServiceNextTrain/GetRealtimeArrivals';
 
 export function handler(event, context, callback) {
