782c47924fd4a3252619a8f5d7b8cd2bad17f2f1
BoyzoE <erick.boyzo@ihg.com>
2019-08-23 09:11:25 -0400

    UI overflow fix

--- src/app/nav-tabs/nav-tabs.component.ts
+++ src/app/nav-tabs/nav-tabs.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
-import { TrainScheduleService } from "../train-schedule-results/train-schedule.service";
-import { filter, map, uniqBy } from "lodash";
-import { LoadingSpinnerService } from "../loading-spinner/loading-spinner.service";
-import { TrainSchedule } from "../train-schedule-results/models/train-schedule";
+import { TrainScheduleService } from '../train-schedule-results/train-schedule.service';
+import { LoadingSpinnerService } from '../loading-spinner/loading-spinner.service';
+import { northSouth, westEast } from './nav-tabs.constants';
 
 @Component({
   selector: 'app-nav-tabs',
@@ -11,23 +10,20 @@ import { TrainSchedule } from "../train-schedule-results/models/train-schedule";
   styleUrls: ['./nav-tabs.component.scss']
 })
 export class NavTabsComponent implements OnInit {
-  northSouthBound: string[] = [];
-  eastWestBound: string[] = [];
-  northSouth = null;
-  eastWest = null;
+  northSouthBound: string[] = northSouth;
+  eastWestBound: string[] = westEast;
+  northSouth: { [key: string]: string } = {primary: 'N', secondary: 'S'};
+  eastWest: { [key: string]: string } = {primary: 'E', secondary: 'W'};
   serviceFailed = false;
 
-  constructor(private trainScheduleService: TrainScheduleService, private loaderService: LoadingSpinnerService) {
+  constructor(private trainScheduleService: TrainScheduleService,
+              private loaderService: LoadingSpinnerService) {
   }
 
   ngOnInit() {
     this.loaderService.show();
     this.trainScheduleService.getTrainSchedule().subscribe(
       data => {
-        this.northSouthBound = map(uniqBy(filter(data, (train: TrainSchedule) => train.DIRECTION === 'S' || train.DIRECTION === 'N'), 'STATION'), 'STATION');
-        this.eastWestBound = map(uniqBy(filter(data, (train: TrainSchedule) => train.DIRECTION === 'E' || train.DIRECTION === 'W'), 'STATION'), 'STATION');
-        this.northSouth = {primary: 'N', secondary: 'S'};
-        this.eastWest = {primary: 'E', secondary: 'W'};
         this.loaderService.hide();
         this.serviceFailed = false;
       },
