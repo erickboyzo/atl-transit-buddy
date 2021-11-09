36beeed4870beb1eae07e5b8eae1fa7f61cd2c90
Erick Boyzo <erickb59@gmail.com>
2021-11-09 09:02:39 -0500

    feat(NG Upgrade) : upgrade from 8 to 9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,24 @@
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {
-  MatButtonModule,
-  MatCardModule,
-  MatDividerModule,
-  MatIconModule,
-  MatInputModule,
-  MatListModule,
-  MatToolbarModule,
-  MatStepperModule,
-  MatTabsModule,
-  MatAutocompleteModule,
-  MatMenuModule,
-  MatProgressSpinnerModule,
-  MatButtonToggleModule,
-  MatChipsModule
-} from '@angular/material';
+
 
 import { AppComponent } from './app.component';
 import { NavTabsComponent } from './nav-tabs/nav-tabs.component';
