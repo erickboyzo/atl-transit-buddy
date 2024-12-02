4195e962e0b97a7bfdd9143db2d1c8b95e8a3b92
Erick Boyzo <erickb59@gmail.com>
2024-12-01 22:38:49 -0500

    fix(build) : update api endpoint

--- src/lambda/realTimeTrain.js
+++ src/lambda/realTimeTrain.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
 
-const apikey = process.env.API_KEY;
+const apiKey = process.env.API_KEY;
 const baseApi = 'https://developerservices.itsmarta.com:18096/itsmarta/railrealtimearrivals/traindata';
 
 export function handler(event, context, callback) {
 
-  axios.get(baseApi, { params: { apikey } })
-.then(res => {
-    callback(null, {
-    statusCode: 200,
-      headers: {
-      'content-type': 'application/json'
-    },
-    body: JSON.stringify(res.data)
-  });
-})
-.catch((error) => {
-    callback(error)
-  });
+  axios.get(baseApi, { params: { apiKey } })
+    .then(res => {
+      callback(null, {
+        statusCode: 200,
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(res.data)
+      });
+    })
+    .catch((error) => {
+      callback(error)
+    });
 }
